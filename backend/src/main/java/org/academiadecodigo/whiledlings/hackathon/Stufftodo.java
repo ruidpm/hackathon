1656788aaeaa7c8bbbb4707f0edf3966a89991c3
Rui Magalhães <ruidpm2@gmail.com>
2019-08-07 15:19:11 +0100

    Initialize the repository; add .gitignore; create basic rest api structure

---
+++ backend/src/main/java/org/academiadecodigo/whiledlings/hackathon/Stufftodo.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.whiledlings.hackathon;
+
+public class Stufftodo {
+
+
+    //// TODO: 07/08/2019 name and app path in pom.xml 
+    // TODO: 07/08/2019 change names and user and pass in properties 
+    // TODO: 07/08/2019 change packages to scan in mysql.xml 
+    //// TODO: 07/08/2019 packages to scan in spring-config.xml 
+    //// TODO: 07/08/2019 check if it works without json way import
+    //// TODO: 07/08/2019 uncomment transaction factory in spring-config.xml 
+}
