43390acc8a4b7389dcc0f731eaa49040bdb1f59c
Rui Magalhães <ruidpm2@gmail.com>
2019-08-08 15:38:56 +0100

    add postDao interface and impl

--- backend/src/main/java/org/academiadecodigo/whiledlings/hackathon/persistence/dao/PostDao.java
+++ backend/src/main/java/org/academiadecodigo/whiledlings/hackathon/persistence/dao/PostDao.java
@@ -1,4 +1,12 @@
 package org.academiadecodigo.whiledlings.hackathon.persistence.dao;
 
 public interface PostDao {
+
+    void delete (Integer id);
+
+    void saveOrUpdate (Post post);
+
+    Post findById (Integer id);
+
+    List<Post> listAll();
 }
---
+++ backend/src/main/java/org/academiadecodigo/whiledlings/hackathon/persistence/dao/PostDaoImpl.java
@@ -0,0 +1,34 @@
+package org.academiadecodigo.whiledlings.hackathon.persistence.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+public class PostDaoImpl implements PostDao{
+
+    private EntityManager em;
+
+    @PersistenceContext
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public void delete(Integer id) {
+        em.remove(em.find(Post.class, id));
+    }
+
+    @Override
+    public void saveOrUpdate(Post post) {
+        em.merge(Post);
+    }
+
+    @Override
+    public Post findById(Integer id) {
+        return em.find(Post.class, id);
+    }
+
+    @Override
+    public List<Post> listAll() {
+        return null;
+    }
+}
