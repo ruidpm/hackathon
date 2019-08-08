8c4ff6d5b79d0857ef516019c76ca247064ef70f
Rui Magalhães <ruidpm2@gmail.com>
2019-08-08 23:31:00 +0100

    merge

--- backend/src/main/java/org/academiadecodigo/whiledlings/hackathon/persistence/model/Post.java
+++ backend/src/main/java/org/academiadecodigo/whiledlings/hackathon/persistence/model/Post.java
@@ -12,39 +12,11 @@ import javax.persistence.Table;
 @Table(name = "post")
 public class Post extends AbstractModel {
 
-    private String name;
-
-    private String email;
-
     private String message;
 
     private String title;
 
-    private String password;
 
-    private Double moneyBanked;
-
-    private String payPallAccount;
-
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * GETTERS AND SETTERS FOR ALL @PARAM
-     */
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     public String getMessage() {
         return message;
@@ -62,27 +34,5 @@ public class Post extends AbstractModel {
         this.title = title;
     }
 
-    public String getPassword() {
-        return password;
-    }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Double getMoneyBanked() {
-        return moneyBanked;
-    }
-
-    public void setMoneyBanked(Double moneyBanked) {
-        this.moneyBanked = moneyBanked;
-    }
-
-    public String getPayPallAccount() {
-        return payPallAccount;
-    }
-
-    public void setPayPallAccount(String payPallAccount) {
-        this.payPallAccount = payPallAccount;
-    }
 }
