ef4731bc9e54ff8d89063d683419f62ed85b3f16
Rui Magalhães <ruidpm2@gmail.com>
2019-08-09 06:48:22 +0100

    add responsive mail

--- backend/src/main/java/org/academiadecodigo/whiledlings/hackathon/controller/rest/RestEmailCtrl.java
+++ backend/src/main/java/org/academiadecodigo/whiledlings/hackathon/controller/rest/RestEmailCtrl.java
@@ -5,12 +5,10 @@ import org.academiadecodigo.whiledlings.hackathon.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("api/sendmail")
 public class RestEmailCtrl {
